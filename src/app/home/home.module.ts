f11c3491b58adf9d0c5fa3ad2d63d412b61d5eda
hotequil <joaohotequil@gmail.com>
2021-01-10 09:29:40 -0300

    criado componente de content, about e services.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,19 @@ import { IntroductionComponent } from './introduction/introduction.component';
 import {BrandModule} from '../brand/brand.module';
 import {ButtonModule} from '../button/button.module';
 import {RouterModule} from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import {ContentModule} from '../content/content.module';
+import { ServicesComponent } from './services/services.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, IntroductionComponent],
+  declarations: [HomeComponent, IntroductionComponent, AboutComponent, ServicesComponent],
     imports: [
         CommonModule,
         BrandModule,
         ButtonModule,
-        RouterModule
+        RouterModule,
+        ContentModule
     ]
 })
 export class HomeModule { }
