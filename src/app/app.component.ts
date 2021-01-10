4501bd00e1f99129cbc4fde0d1e8d337eb8bf268
hotequil <joaohotequil@gmail.com>
2021-01-10 18:08:19 -0300

    adicionado jquery para suavizar redirecionamento interno.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+
+import * as $ from 'jquery';
+import ClickEvent = JQuery.ClickEvent;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{}
+export class AppComponent implements AfterViewInit{
+    ngAfterViewInit(): void{
+        $(() => {
+            $('a').on('click', (event: ClickEvent) => {
+                const hash = event.currentTarget.hash || event.target.hash;
+
+                if (hash !== '') {
+                    event.preventDefault();
+
+                    $('html, body').animate({
+                        scrollTop: $(hash)?.offset?.()?.top
+                    }, 1000);
+                }
+            });
+        });
+    }
+}
