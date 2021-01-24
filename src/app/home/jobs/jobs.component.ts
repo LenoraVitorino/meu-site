080bcb98dd0dfd1ab5c58809d3fa8403b25db5e7
hotequil <joaohotequil@gmail.com>
2021-01-24 18:35:20 -0300

    finalizada parte de jobs.

--- src/app/home/jobs/jobs.component.ts
+++ src/app/home/jobs/jobs.component.ts
@@ -12,7 +12,7 @@ export class JobsComponent{
         { src: './assets/images/freedom-lingerie.jpg', href: '', text: 'Freedom Lingerie' },
         { src: './assets/images/bell-beach-wear.jpg', href: '', text: 'Bell Beach Wear' },
         { src: './assets/images/ketlin-andriele-makeup.jpg', href: '', text: 'Ketlin Andriele Makeup' },
-        { src: './assets/images/bell-beach-wear.jpg', href: '', text: 'La Linda Boutique' },
-        { src: './assets/images/bell-beach-wear.jpg', href: '', text: 'Lenóra Vitorino Design' }
+        { src: './assets/images/lenora-vitorino.jpg', href: '', text: 'Lenóra Vitorino Design' },
+        { src: './assets/images/la-linda-boutique.jpg', href: '', text: 'La Linda Boutique' }
     ];
 }
